8af3659ca02509f81603c813cef483eb79d83687
Rohan Mathur <rmathur101@gmail.com>
2023-01-19 17:58:04 -0600

    simplify qa file

--- run_tns_qa.py
+++ run_tns_qa.py
@@ -1,23 +1,33 @@
-from dotenv import load_dotenv
-import numpy as np
-import openai
-import pandas as pd
-import pickle
-import tiktoken
+from helpers import answer_query_with_context
 import os
+import json
 
-load_dotenv()
+with open('embeddings_map.json', 'r') as f:
+  embeddings_map = json.load(f)
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+with open('all_chunks_map.json', 'r') as f:
+  all_chunks_map = json.load(f)
 
-COMPLETIONS_MODEL = "text-davinci-003"
-EMBEDDING_MODEL = "text-embedding-ada-002"
+# query = "Can you give me some specific examples of what countries are doing today that are preventing innovation from happening?"
+# query = "What are the steps to create a network state?"
+# query = "Why do we need network states?"
+# query = "What is a startup society and how do you found one?"
+# query = "Can you give me an example of a startup society and how it was founded?"
+# query = "What do you see the future looking like in the next 10 or 20 year?"
+# query = "What kind of new technologies or ways of living will be possible in a network state?"
+# query = "How do you organize a group capable of collective action? Can you give me an example?"
+# query = "Can you give me an example of a network union?"
+# query = "What does the future of media look like? How will network states change the future of media?"
+# query = "What are your thoughts about the new york times?"
+# query = "Can you give me an example of how the NYT started a war?"
+# query = "How can I get involved in the network state movement?"
+# query = "What role does crypto and blockchain play in the creation of network states?"
+# query = "What are your thoughts about China and the future of China?"
+# query = "What are the most important and contrarian ideas in the book?"
+# query = "What is the concept of the ledger of record and why is it important?"
+# query = "What is an example of how the ledger of record will be used to build network states?"
+# query = "What are the different ways that liberals, convservatives, and libertarians view the world and will impact the future?"
+query = "What technologies will be most important in the future?"
 
-prompt = "Who won the 2020 Summer Olympics men's high jump?"
-
-openai.Completion.create(
-    prompt=prompt,
-    temperature=0,
-    max_tokens=300,
-    model=COMPLETIONS_MODEL
-)["choices"][0]["text"].strip(" \n")
+answer = answer_query_with_context(query, all_chunks_map, embeddings_map, show_prompt=True)
+print(answer)
